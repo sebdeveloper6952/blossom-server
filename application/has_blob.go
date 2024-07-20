c449d3eca2dbae26b5c64f2383a7ff7807317e83
sebas <sebdeveloper6952@gmail.com>
2024-07-20 11:51:36 -0600

    refactor app logic handlers

--- application/has_blob.go
+++ application/has_blob.go
@@ -2,19 +2,14 @@ package application
 
 import (
 	"context"
+
 	"github.com/sebdeveloper6952/blossom-server/domain"
 )
 
 func HasBlob(
-	blobDescriptorRepo domain.BlobDescriptorRepo,
-) func(
 	ctx context.Context,
+	blobRepo domain.BlobDescriptorRepo,
 	sha256 string,
 ) (bool, error) {
-	return func(
-		ctx context.Context,
-		sha256 string,
-	) (bool, error) {
-		return blobDescriptorRepo.Exists(ctx, sha256)
-	}
+	return blobRepo.Exists(ctx, sha256)
 }
